3fb24694a0c56e73fce35da8a154f88932d61ed4
CrazyQ <crazyq@gmail.com>
2026-03-02 11:03:38 +0800

    Update and rename build_ucrt64_qt5-static.sh

---
+++ build_ucrt64_qt5-static.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "$(dirname "$0")" >/dev/null
+if [[ ! -d "build" ]]; then mkdir "build"; fi
+cd build
+rm -r -f * .*
+
+/ucrt64/qt5-static/bin/qmake.exe .. \
+    USE_SDL_GAMEPAD=1 \
+    SDL_LIBS="-lmingw32 -mwindows -lSDL2 -lm -lkernel32 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lversion -luuid -ladvapi32 -lsetupapi -lshell32 -ldinput8 -Wl,--no-undefined" \
+    SDL_INCLUDES="/ucrt64/include/SDL2" \
+    INSTALLDIR="$PWD/dist"
+
+if [ $? -eq 0 ]; then make -j; fi
+if [ $? -eq 0 ]; then make install; fi
+cd ..
+exit $?
+
--- build_ucrt64_static.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/ucrt64/qt5-static/bin/qmake.exe .. \
-USE_SDL_GAMEPAD=1 \
-SDL_LIBS="-lmingw32 -mwindows -lSDL2 -lm -lkernel32 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lversion -luuid -ladvapi32 -lsetupapi -lshell32 -ldinput8 -Wl,--no-undefined" \
-SDL_INCLUDES="/ucrt64/include/SDL2" \
-INSTALLDIR="$PWD/dist"
