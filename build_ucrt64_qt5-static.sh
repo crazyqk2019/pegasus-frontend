b718120b7b551f48074e6a77b3903bce131a25b3
CrazyQ <crazyq@gmail.com>
2026-03-03 16:00:57 +0800

    Update build_ucrt64_qt5-static.sh to use pkg-config get sdl2 libs and includes

--- build_ucrt64_qt5-static.sh
+++ build_ucrt64_qt5-static.sh
@@ -7,8 +7,8 @@ rm -r -f * .*
 
 /ucrt64/qt5-static/bin/qmake.exe .. \
     USE_SDL_GAMEPAD=1 \
-    SDL_LIBS="-lmingw32 -mwindows -lSDL2 -lm -lkernel32 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lversion -luuid -ladvapi32 -lsetupapi -lshell32 -ldinput8 -Wl,--no-undefined" \
-    SDL_INCLUDES="/ucrt64/include/SDL2" \
+    SDL_LIBS="`pkg-config --libs --static sdl2 | sed 's/\s*-lSDL2main\b//g'` -Wl,--no-undefined" \
+    SDL_INCLUDES="`pkg-config --cflags-only-I --static sdl2 | sed 's/s*-I//g'`" \
     INSTALLDIR="$PWD/dist"
 
 if [ $? -eq 0 ]; then make -j; fi
